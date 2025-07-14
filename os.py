4530725d92a0c39e721d9b2288fb202fdeda8fc2
Hiter666-Join <3557305035@qq.com>
2025-07-15 03:19:59 +0800

    Update os.py

--- os.py
+++ os.py
@@ -1,9 +1,9 @@
 import os
 
-data= os.path.dirname(__file__)  #os.path.dirname打开当前目录文件夹
-file_path= os.path.join(data,'hi.txt')  #os.path.join加上上面提取的目录用逗号拼接目录
+data= os.path.dirname(__file__)  
+file_path= os.path.join(data,'hi.txt')  
 
-with open(file_path,'r',encoding= 'utf-8') as f:  #open打开
+with open(file_path,'r',encoding= 'utf-8') as f:  
     content= f.read()
 
 print(content)
